a899c6e5ff80991a8f4e47ab225a85740f6af1a4
miya52352 <firkej648lh78@gmail.com>
2024-06-27 11:49:06 +0900

    20240627

--- JS/main.js
+++ JS/main.js
@@ -1,43 +1,78 @@
-// スムーススクロール（※バージョン2024-1）※通常タイプ
-$(function() {
-    // ページ上部へ戻るボタンのセレクター
-    var topButton = $('.pagetop');
-    // ページトップボタン表示用のクラス名
-    var scrollShow = 'pagetop-show';
+const EXERCISE_KEY = "KkHBDdhaF0sYyFSVba2FGQ==RXU731Fltx4dcRhc";
+const EXERCISE_URL = "https://api.api-ninjas.com/v1/exercises";
 
-    // スムーススクロールを実行する関数
-    // targetにはスクロール先の要素のセレクターまたは'#'（ページトップ）を指定
-    function smoothScroll(target) {
-        // スクロール先の位置を計算（ページトップの場合は0、それ以外は要素の位置）
-        var scrollTo = target === '#' ? 0 : $(target).offset().top;
-        // アニメーションでスムーススクロールを実行
-        $('html, body').animate({scrollTop: scrollTo}, 500);
-    }
+const DEEPL_KEY = "2c972989-c128-48cf-a45e-07bd7b8ac875:fx";
+const DEEPL_URL = 'https://api-free.deepl.com/v2/translate';
 
-    // ページ内リンクとページトップへ戻るボタンにクリックイベントを設定
-    $('a[href^="#"], .pagetop').click(function(e) {
-        e.preventDefault(); // デフォルトのアンカー動作をキャンセル
-        var id = $(this).attr('href') || '#'; // クリックされた要素のhref属性を取得、なければ'#'
-        smoothScroll(id); // スムーススクロールを実行
+
+async function getExerciseMenus(params) {
+  const query_params = new URLSearchParams(params); 
+  console.log(`${EXERCISE_URL}?` + query_params)
+  
+    let response = await fetch(`${EXERCISE_URL}?` + query_params, {
+      headers: {
+        'X-Api-Key': EXERCISE_KEY,
+      }
     });
 
-    // スクロールに応じてページトップボタンの表示/非表示を切り替え
-    $(topButton).hide(); // 初期状態ではボタンを隠す
-    $(window).scroll(function() {
-        if($(this).scrollTop() >= 300) { // スクロール位置が300pxを超えたら
-            $(topButton).fadeIn().addClass(scrollShow); // ボタンを表示
-        } else {
-            $(topButton).fadeOut().removeClass(scrollShow); // それ以外では非表示
-        }
-    });
+    let result = await response.json();
 
-    // ページロード時にURLのハッシュが存在する場合の処理
-    if(window.location.hash) {
-        // ページの最上部に即時スクロールする
-        $('html, body').scrollTop(0);
-        // 少し遅延させてからスムーススクロールを実行
-        setTimeout(function() {
-            smoothScroll(window.location.hash);
-        }, 10);
-    }
+    console.log("Exercise: " + result);
+
+    return await result;
+}
+
+
+
+
+// DEEPL API
+async function getDeepLText(input_text) {
+
+  let content = encodeURI('auth_key=' + DEEPL_KEY + '&text=' + input_text + '&source_lang=EN&target_lang=JA');
+  let url = DEEPL_URL + '?' + content;
+
+    let response = await fetch(url)
+    let result = await response.json();
+
+    console.log(result);
+
+    return await result;
+};
+
+
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  let myButton = document.getElementById("myButton");
+  let result = document.getElementById("result");
+  let translation_result = document.getElementById("translation_result");
+
+
+  if (myButton) {
+    myButton.addEventListener("click", async function() {
+      result.textContent=""
+      translation_result.textContent=""
+      const params = { // 渡したいパラメータをJSON形式で書く
+        muscle: document.getElementById("muscle-groups").value,
+        type:document.getElementById("exercise-type").value,
+        difficulty:document.getElementById("level").value,
+      };
+
+      console.log(params);
+    
+      // Exercise API
+      let exercises =  await getExerciseMenus(params);
+
+      if(exercises.length>0){
+      result.textContent = exercises[0]["instructions"];
+      console.log(exercises[0])
+      // DeepL API
+      let deepl = await getDeepLText(exercises[0]["instructions"]);
+      translation_result.textContent = deepl["translations"][0]["text"];
+      }
+      else{
+        result.textContent="該当するトレーニングは存在しません。別のトレーニングタイプ・鍛えたい部位・レベルを選択してください"
+      }
+    });
+    
+  }
 });
